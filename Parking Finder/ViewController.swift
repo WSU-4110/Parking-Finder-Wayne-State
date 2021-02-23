9b703742a16f091eb80bffd9dd1f4780feacf5c8
Fahim-007 <78050551+Fahim-007@users.noreply.github.com>
2021-02-23 18:28:25 -0500

    Update ViewController.swift

--- Parking Finder/ViewController.swift
+++ Parking Finder/ViewController.swift
@@ -4,20 +4,85 @@
 //
 //  Created by Server on 2/7/21.
 //
-
 import UIKit
 import FirebaseDatabase
 
-class homeViewController: UIViewController
-{
-
+class homeViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate{
+    private let database = Database.database().reference()
+    
+    // Outlets for text fields and button
+    @IBOutlet weak var userParkedZoneField: UITextField!
+    @IBOutlet weak var userParkedETimeField: UITextField!
+    
+    // Label that is red and will be green when user online
+    @IBOutlet weak var notifyUserOnline: UILabel!
+    
+   
     override func viewDidLoad()
     {
-        
-        
         super.viewDidLoad()
+    
+        userParkedZoneField.delegate = self
+        userParkedETimeField.delegate = self
+        
+        // code make i change colors - notifyUserOnline.textColor = UIColor.blue
+    }
+    // Eli write code to naviagate user to login View controller, similar to didTapCreateProfileButton() but instead of button do the VIEW element that holds all elements
+    override func viewDidAppear(_ animated: Bool) { self.performSegueWithIdentifier("loginView", sender: self);
+    }
+    
+    
+    
+    // Fahim write code to naviagate user to create profile View controller, similar to didTapCreateProfileButton() but instead of button do the VIEW element that holds all elements
+    }
+     override func viewDidAppear(_ animated: Bool) { self.performSegueWithIdentifier("createView", sender: self);
+    }
+    
+    
+    
+    // Action to send user parked data to database where user is
+    @IBAction func parkedButton(_ sender: Any) {
+        let createProfileObject: [String: Any] =
+        [
+            "parkingZone":userParkedZoneField.text!,
+            "parkingET":userParkedETimeField.text!
+        ]
+        database.child("Users").childByAutoId().setValue(createProfileObject)
+    }
+    
+    // Func Handle textfields Home Page
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        // code that allows users to enter only numbers
+        let allowedCharacters = "+1234567890"
+        let allowedCharacterSet = CharacterSet(charactersIn: allowedCharacters)
+        let typedCharacterSet = CharacterSet(charactersIn: string)
+        
+        // code that allows users to enter three numbers total
+        let maxLength = 3
+        let currentString: NSString = (textField.text ?? "") as NSString
+        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        
+        return allowedCharacterSet.isSuperset(of: typedCharacterSet) && newString.length <= maxLength
     }
 
+    
+    //keyboard methods for fields
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        hidekeyboardHome()
+        return true
+    }
+    
+    // Func hideKeyboard
+    func hidekeyboardHome()
+    {
+        print("Hide Keyboard")
+        userParkedZoneField.resignFirstResponder()
+        userParkedETimeField.resignFirstResponder()
+    }
+
+    
 }
 class accountViewController: UIViewController
 {
