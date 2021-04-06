717b909c66d11cc8895106aa175ec7085ad163dd
Garrett Berger <77683097+GarrettBWayneState@users.noreply.github.com>
2021-04-06 10:27:56 -0400

    Map Annotation buttons added
    Implemented couple functions for map annotations. First, added info buttons, and second added color method for notifying users.

--- Parking Finder/ViewController.swift
+++ Parking Finder/ViewController.swift
@@ -48,7 +48,7 @@ class homeViewController: UIViewController, UITableViewDelegate, UITextFieldDele
         }
     }*/
     
-    
+
     
     // Eli's Code to log users out
     @IBAction func logOut_Button(_ sender: Any)
@@ -75,8 +75,6 @@ class homeViewController: UIViewController, UITableViewDelegate, UITextFieldDele
     @IBAction func homeButton(_ sender: Any)
     {
         
-        
-
     }
     
 
@@ -138,58 +136,34 @@ class homeViewController: UIViewController, UITableViewDelegate, UITextFieldDele
 
     
 }
-class finderViewController: UIViewController
-{
 
+
+
+
+
+
+
+class finderViewController: UIViewController, MKMapViewDelegate
+{
     @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-  
-        getParkzonesOnMap(wayneParkingZones)
+        mapView.delegate = self
         setZoomLevel(location: locationLatLong)
-        
+        self.placePins()
     }
     
-    
-    // Eli Code Logout Button
-    @IBAction func logOut_FinderButton(_ sender: Any)
-    {
-        do
-        {
-            try Auth.auth().signOut()
-        }
-        catch let LogoutError
-        {
-            print(LogoutError)
-        }
-       // print(Auth.auth().currentUser)
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let signInVC = storyboard.instantiateViewController(withIdentifier: "loginViewController")
-        self.present(signInVC, animated: true, completion: nil)
-    }
-    
-    
-    
-    
-    // initial start coordinates
-    let locationLatLong = CLLocation(latitude: 42.354405249, longitude: -83.0687297251)
-    
-    // initial start radius distance
-    let distanceSpan: CLLocationDistance = 1500
-    
-    // create region for zoom level
-    func setZoomLevel(location: CLLocation)
+    // Configure map starting coorndinates
+    let locationLatLong = CLLocation(latitude: 42.354405249, longitude: -83.0687297251)                                                                 // initial start coordinates
+    let distanceSpan: CLLocationDistance = 1500                                                                                                         // initial start radius distance
+    func setZoomLevel(location: CLLocation)                                                                                                             // create region for zoom level
     {
         let mapCoordinates = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: distanceSpan, longitudinalMeters: distanceSpan)
         mapView.setRegion(mapCoordinates, animated: true)
     }
-
-    
-    
-    
-    
     
     // structure for annotation pins
     struct ParkingZone
@@ -198,39 +172,148 @@ class finderViewController: UIViewController
         var lattitude: CLLocationDegrees
         var longtitude: CLLocationDegrees
     }
+
     
-    // set each pin coordinate and name
-    let wayneParkingZones = [ParkingZone(name: "\"202\"", lattitude: 42.35722, longtitude: -83.06361111),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35689, longtitude: -83.06472222),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35670, longtitude: -83.06530),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35635, longtitude: -83.06611111),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35605, longtitude: -83.06694444),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35577, longtitude: -83.06777778),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35559, longtitude: -83.06833333),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35525, longtitude: -83.06944444),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35515, longtitude: -83.06972222),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35490, longtitude: -83.07055556),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35455, longtitude: -83.07166667),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35420, longtitude: -83.07277778),
-                             
-                             ParkingZone(name: "\"206\"", lattitude: 42.35445, longtitude: -83.06580),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35390, longtitude: -83.06555556),
-                             ParkingZone(name: "\"206\"", lattitude: 42.35420, longtitude: -83.07277778)
-    ]
     
-    // add each annotation parking zone with its info
-    func getParkzonesOnMap(_ wayneParkingZones: [ParkingZone])
+    let detailLabel = UILabel()
+
+    
+    func placePins()
     {
-        for ParkingZone in wayneParkingZones
+        // set each pin coordinate and name
+        let wayneParkingZonesC = [ParkingZone(name: "1", lattitude: 42.35722, longtitude: -83.06361111),
+                                 ParkingZone(name: "2", lattitude: 42.35689, longtitude: -83.06472222),
+                                 ParkingZone(name: "3", lattitude: 42.35670, longtitude: -83.06530),
+                                 ParkingZone(name: "4", lattitude: 42.35635, longtitude: -83.06611111),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35605, longtitude: -83.06694444),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35577, longtitude: -83.06777778),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35559, longtitude: -83.06833333),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35525, longtitude: -83.06944444),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35515, longtitude: -83.06972222),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35490, longtitude: -83.07055556),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35455, longtitude: -83.07166667),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35420, longtitude: -83.07277778),
+                                 
+                                 ParkingZone(name: "\"207\"", lattitude: 42.35445, longtitude: -83.06580),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35390, longtitude: -83.06555556),
+                                 ParkingZone(name: "\"206\"", lattitude: 42.35420, longtitude: -83.07277778)
+        ]
+        // add each annotation parking zone with its info
+        for ParkingZone in wayneParkingZonesC
         {
-            let annotations = MKPointAnnotation()
-            annotations.title = ParkingZone.name
-            annotations.coordinate = CLLocationCoordinate2D(latitude: ParkingZone.lattitude, longitude: ParkingZone.longtitude)
-            mapView.addAnnotation(annotations)
-            
+            let annotation = MKPointAnnotation()
+            annotation.title = ParkingZone.name
+            annotation.coordinate = CLLocationCoordinate2D(latitude: ParkingZone.lattitude, longitude: ParkingZone.longtitude)
+            mapView.addAnnotation(annotation)
         }
     }
+
     
+/*
+var markerTintColor: UIColor
+{
+    switch annotation.title!!
+    {
+        case "Monument":
+            return .red
+        case "Mural":
+            return .cyan
+        case "Plaque":
+            return .blue
+        case "Sculpture":
+            return .purple
+        default:
+            return .green
+    }
+}*/
+func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+{
+
+    /*let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "MyMarker")                              // change colors code
+    
+    switch annotation.title
+    {
+        case "1":                                                               // add available func
+            annotationView.markerTintColor = UIColor.green
+        case "2":                                                                // add extended func
+            annotationView.markerTintColor = UIColor.blue
+        case "3":                                                                 // add full func
+            annotationView.markerTintColor = UIColor.red
+        default:                                                                   // if nothing than be grey
+            annotationView.markerTintColor = UIColor.gray
+    }*/
+    
+    let annotationIdentifier = "MyMarker"
+    var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+    
+    /*var image: UIImage
+    {
+      guard let name = discipline
+    else
+    {
+        return #imageLiteral(resourceName: "Flag")
+    }*/
+    
+    if annotationView == nil
+    {
+        annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+        annotationView!.canShowCallout = true
+        
+        //let mapsButton = UIButton(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 48, height: 48)))
+       // mapsButton.setBackgroundImage(#, for: .normal)
+        
+        annotationView!.annoDesign(arrangedSubviews: [UIButton(type: .detailDisclosure), UIButton(type: .detailDisclosure), UIButton(type: .detailDisclosure)])     // three added info buttons
+    }
+    else
+    {
+        annotationView!.annotation = annotation
+    }
+    return annotationView
+}
     
 
+    
 }
+
+
+
+extension MKAnnotationView                                                                              // design of annotation container
+{
+
+    func annoDesign(arrangedSubviews: [UIView])
+    {
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin, .flexibleLeftMargin, .flexibleBottomMargin, .flexibleWidth, .flexibleHeight]
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+        
+        self.detailCalloutAccessoryView = stackView
+    }
+}
+
+/*extension finderViewController
+{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        let annotationIdentifier = "MyMarker"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+
+        if annotationView == nil
+        {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView!.canShowCallout = true
+            annotationView!.annoDesign(arrangedSubviews: [UIButton(type: .detailDisclosure), UIButton(type: .detailDisclosure), UIButton(type: .detailDisclosure)])
+        }
+        else
+        {
+            annotationView!.annotation = annotation
+        }
+        return annotationView
+    }
+    
+}*/
+
